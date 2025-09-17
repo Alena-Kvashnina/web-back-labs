37cbed821c5ae3198802d3822429bedb86004ca7
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-09-17 22:35:11 +0700

    Первое приложение

--- app.py
+++ app.py
@@ -2,11 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/web")
 def start():
-    return """<!doctype html> 
-        <html> 
-            <body> 
-                <h1>web-сервер на flask</h1> 
-            </body> 
-        </html>"""
+    return "web-сервер на flask"
