670cce09714a4c3b215128e44bc2a301424e0972
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-09-19 17:11:24 +0700

    Исправление адресов

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def not_found(err):
     return "нет такой страницы", 404 
     
 @app.route("/")
-@app.route("/web")
+@app.route("/lab1/web")
 def web():
     return """<!doctype html> 
         <html>
@@ -19,7 +19,7 @@ def web():
             'Content-Type': 'text/plain; charset=utf-8'
         }
         
-@app.route("/author")
+@app.route("/lab1/author")
 def author():
     name = "Квашнина Алёна Юрьевна"
     group = "ФБИ-34"
@@ -31,11 +31,11 @@ def author():
               <p>Студент: """ + name + """</p>
               <p>Группа: """ + group + """</p>
               <p>Факультет: """ + faculty + """</p>
-              <a href="/web">web</a>
+              <a href="/lab1/web">web</a>
             </body>
         </html>"""
 
-@app.route('/image')
+@app.route('/lab1/image')
 def image():
     path = url_for("static", filename="oak.jpg")
     css = url_for("static", filename="lab1.css")
@@ -52,7 +52,7 @@ def image():
 
 count = 0
 
-@app.route('/counter')
+@app.route('/lab1/counter')
 def counter():
     global count
     count += 1
@@ -68,11 +68,11 @@ def counter():
                 <br> Дата и время: ''' + time + '''
                 <br> Запрошенный адрес: ''' + url + '''
                 <br> Ваш IP адрес: ''' + client_ip + '''
-                <br><a href="/counter/reset">Сбросить счётчик</a>
+                <br><a href="/lab1/counter/reset">Сбросить счётчик</a>
             </body>
         </html>'''
 
-@app.route('/counter/reset')
+@app.route('/lab1/counter/reset')
 def reset_counter():
     global count
     count = 0
@@ -80,13 +80,13 @@ def reset_counter():
         <html>
             <body>
                 <h1>Счётчик обнулён!</h1>
-                <a href="/counter">Назад к счётчику</a>
+                <a href="/lab1/counter">Назад к счётчику</a>
             </body>
         </html>'''
 
-@app.route('/info')
+@app.route('/lab1/info')
 def info():
-    return redirect("/author")
+    return redirect("/lab1/author")
 
 @app.route("/lab1/created")
 def created():
