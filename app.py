30a8867943771f67aa0c12223f66687be39a4560
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-09-19 17:34:36 +0700

    Главная страница

--- app.py
+++ app.py
@@ -2,11 +2,40 @@ from flask import Flask, url_for, request, redirect
 import datetime
 app = Flask(__name__)
 
+
+@app.route("/")
+@app.route("/index")
+def index():
+    lab1_web = url_for("web")
+    return f'''
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>НГТУ, ФБ, Лабораторные работы</title>
+    </head>
+    <body>
+        <header>
+            <h1>НГТУ, ФБ, WEB-программирование, часть 2. Список лабораторных</h1>
+            <hr>
+        </header>
+        <main>
+            <ul>
+                <li><a href="{lab1_web}">Первая лабораторная</a></li>
+            </ul>
+        </main>
+        <footer>
+            <hr>
+            <p>Квашнина Алёна Юрьевна, ФБИ-34, 3 курс, 2025 год</p>
+        </footer>
+    </body>
+</html>
+'''
+
 @app.errorhandler(404)
 def not_found(err):
     return "нет такой страницы", 404 
-    
-@app.route("/")
+
 @app.route("/lab1/web")
 def web():
     return """<!doctype html> 
