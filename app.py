6a02240911a4ec8145774ef498bbaf9e553ffbc3
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-09-30 23:44:51 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -353,4 +353,5 @@ def add_flower(name):
 '''
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Алена Квашнина'
+    return render_template('example.html', name=name)
