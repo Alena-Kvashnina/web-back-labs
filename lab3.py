29f198ede392e7b73978fef9c9a7a888037bfdc9
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-10-16 14:35:55 +0700

    Форма

--- lab3.py
+++ lab3.py
@@ -1,8 +1,35 @@
-from flask import Blueprint, render_template, routes
+from flask import Blueprint, render_template, request, make_response, redirect
 lab3 = Blueprint('lab3', __name__)
 
 
 @lab3.route('/lab3/')
-def lab():
-    return render_template('lab3.html', routes=routes)
+def lab33():
+    name = request.cookies.get('name')
+    name_color = request.cookies.get('name_color')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
 
+
+@lab3.route('/lab3/cookie')
+def cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Alex', max_age=5)
+    resp.set_cookie('age', '20')
+    resp.set_cookie('name_color', 'magenta')
+    return resp
+
+
+@lab3.route('/lab3/clear_cookie')
+def clear_cookie():
+    resp = make_response(redirect('/lab3/'))
+    resp.set_cookie('name', '', expires=0)
+    resp.set_cookie('age', '', expires=0)
+    resp.set_cookie('name_color', '', expires=0)
+    return resp
+
+
+@lab3.route('/lab3/form1')
+def form1():
+    user = request.args.get('user')
+    age = request.args.get('age')
+    sex = request.args.get('sex')
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
