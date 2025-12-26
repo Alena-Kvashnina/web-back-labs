ba465ba0a93da74fe16ea76dc2d0028a36ba07f1
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-12-26 20:40:46 +0700

    Регистрация

--- lab8.py
+++ lab8.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request, redirect
+from db import db
+from db.models import users, articles
+
 
 lab8 = Blueprint('lab8', __name__)
 
@@ -10,9 +13,25 @@ def lab88():
 def login():
     return "Страница входа"
 
-@lab8.route('/lab8/register')
+@lab8.route('/lab8/register', methods=['GET', 'POST'])
 def register():
-    return "Страница регистрации"
+    if request.method == 'GET':
+        return render_template('lab8/register.html')
+    login_form = request.form.get('login')
+    password_form = request.form.get('password')
+
+    login_exists = users.query.filter_by(login = login_form).first()
+    if login_exists:
+        return render_template('lab8/register.html', error='Такой пользователь уже существует')
+    
+
+    password_hash = generate_password_hash(password_form)
+    new_user = users(login = login_form, password = password_hash)
+    db.session.add(new_user)
+    db.session.commit()
+    login_user(new_user)
+    return redirect('/lab8/')
+
 
 @lab8.route('/lab8/articles')
 def articles():
