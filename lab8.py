7d8d8c9dec57c23100c03f6ac4e5a818de54a1fc
Алена Квашнина <alena.kvashnina05@mail.ru>
2025-12-26 20:21:27 +0700

    Модель базы данных

--- lab8.py
+++ lab8.py
@@ -1,28 +1,23 @@
-from flask import Blueprint, render_template
-from flask_login import current_user
+from flask import Blueprint, render_template, session
 
 lab8 = Blueprint('lab8', __name__)
 
 @lab8.route('/lab8/')
-def lab():
-    # Получаем имя пользователя или 'anonymous'
-    username = current_user.login if current_user.is_authenticated else 'anonymous'
-    return render_template('lab8/lab8.html', username=username)
+def lab88():  
+    return render_template('lab8/lab8.html', username='Anonymous')
 
 @lab8.route('/lab8/login')
 def login():
-    return render_template('lab8/lab8.html', username='anonymous', page='login')
+    return "Страница входа"
 
 @lab8.route('/lab8/register')
 def register():
-    return render_template('lab8/lab8.html', username='anonymous', page='register')
+    return "Страница регистрации"
 
 @lab8.route('/lab8/articles')
 def articles():
-    username = current_user.login if current_user.is_authenticated else 'anonymous'
-    return render_template('lab8/lab8.html', username=username, page='articles')
+    return "Список статей"
 
 @lab8.route('/lab8/create')
 def create():
-    username = current_user.login if current_user.is_authenticated else 'anonymous'
-    return render_template('lab8/lab8.html', username=username, page='create')
+    return "Создать статью"
